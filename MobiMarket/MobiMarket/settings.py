77a6c055f27aeec24093ece25775cb4ccfce7a0c
mir21bek <mirbek.2121@gmail.com>
2023-09-26 12:47:25 +0600

    update settings

--- MobiMarket/MobiMarket/settings.py
+++ MobiMarket/MobiMarket/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MobiMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT')
     }
 }
 
