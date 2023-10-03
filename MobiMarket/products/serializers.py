d7b5bd55e5a8e91fd2b365fac1254ce0f7bd330c
mir21bek <mirbek.2121@gmail.com>
2023-10-04 00:28:32 +0600

    update the serializers.py

--- MobiMarket/products/serializers.py
+++ MobiMarket/products/serializers.py
@@ -11,10 +11,16 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class MyProductSerializers(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    name = serializers.CharField(max_length=255)
+    description = serializers.CharField(max_length=500)
+    short_description = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=7, decimal_places=2)
+    photo = serializers.ImageField()
+    available = serializers.BooleanField(default=True)
 
     class Meta:
         model = MyProduct
-        fields = ('user', 'product')
+        fields = ('user', 'name', 'description', 'available', 'photo', 'short_description', 'price')
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
